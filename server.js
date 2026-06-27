2be829363a76e145cc9a63c59a0a5410ec25361f
naderlabib-afk <nader.labib@gmail.com>
2026-06-28 01:53:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const fetch = require('node-fetch');
 
 const app = express();
 app.use(cors({
-  origin: '*',
+  origin: function(origin, callback) {
+    console.log('[CORS] Origin:', origin);
+    callback(null, origin || '*');
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 const port = process.env.PORT || 3000;
 const dataRoot = path.join(__dirname, 'data');
